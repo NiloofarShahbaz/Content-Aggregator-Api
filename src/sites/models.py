5dcfcd44988befc99a94026f7869a3543cd624ca
Niloofar Shahbaz <niloofar1376@gmail.com>
2019-10-14 20:07:25 +0330

    Add sites models

--- src/sites/models.py
+++ src/sites/models.py
@@ -1,2 +1,21 @@
 from django.db import models
+from django.conf import settings
 
+
+class Site(models.Model):
+    name = models.CharField(max_length=150, unique=True)
+    url = models.URLField()
+    icon = models.ImageField()
+    users = models.ManyToManyField(to=settings.AUTH_USER_MODEL)
+
+    def __str__(self):
+        return self.name
+
+
+class Link(models.Model):
+    title = models.TextField()
+    url = models.URLField()
+    site = models.ForeignKey(to=Site, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return str(self.site) + self.title
